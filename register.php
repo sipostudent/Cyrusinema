57b3cb460e5a3f2cd3829920912c8621d41c43a1
Sipo Charles <sipocharles@yahoo.co.uk>
2020-04-09 15:03:04 +0100

    first commit

---
+++ register.php
@@ -0,0 +1,43 @@
+<?php
+    if(isset($_POST["submitButton"])) {
+        echo "Form was submitted";
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Cyrusinema</title>
+    <link rel="stylesheet" type="text/css" href="assets/style/style.css" />
+</head>
+<body>
+    <div class="signInContainer">
+
+        <div class="column">
+
+            <form method="POST">
+
+                <input type="text" name="firstName" placeholder="First name" required>
+
+                <input type="text" name="lastName" placeholder="Last name" required>
+
+                <input type="text" name="userName" placeholder="User name" required>
+
+                <input type="email" name="email" placeholder="Email" required>
+
+                <input type="email" name="email2" placeholder="Confirm email" required>
+
+                <input type="password" name="password" placeholder="Password" required>
+
+                <input type="password2" name="password2" placeholder="Confirm password" required>
+
+                <input type="submit" name="submitButton" value="  SUBMIT">
+            
+            </form>
+
+        </div>
+
+    </div>
+</body>
+</html>
