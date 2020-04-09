1b741325fb81d5125b3b0feccc2f35b6e2c3be4e
Sipo Charles <sipocharles@yahoo.co.uk>
2020-04-09 16:04:15 +0100

    Added site logo

--- register.php
+++ register.php
@@ -4,45 +4,46 @@
     }
 ?>
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to Cyrusinema</title>
-    <link rel="stylesheet" type="text/css" href="assets/style/style.css" />
-</head>
-<body> 
-    <div class="signInContainer">
+<html>
+    <head>
+        <title>Welcome to Cyrusinema</title>
+        <link rel="stylesheet" type="text/css" href="assets/style/style.css" />
+    </head>
+    <body>
+        
+        <div class="signInContainer">
 
-        <div class="column">
+            <div class="column">
 
-            <div class="header">
-                <h3>Sign Up</h3>
-                <span>to continue to Cyrusinema</span>
+                <div class="header">
+                    <img src="assets/images/cyrusinema-logo.png" title="logo" alt="Site logo">
+                    <h3>Sign Up</h3>
+                    <span>to continue to Cyrusinema</span>
+                </div>
+
+                <form method="POST">
+
+                    <input type="text" name="firstName" placeholder="First name" required>
+
+                    <input type="text" name="lastName" placeholder="Last name" required>
+
+                    <input type="text" name="username" placeholder="Username" required>
+
+                    <input type="email" name="email" placeholder="Email" required>
+
+                    <input type="email" name="email2" placeholder="Confirm email" required>
+
+                    <input type="password" name="password" placeholder="Password" required>
+
+                    <input type="password" name="password2" placeholder="Confirm password" required>
+
+                    <input type="submit" name="submitButton" value="SUBMIT">
+
+                </form>
+
             </div>
-
-            <form method="POST">
-
-                <input type="text" name="firstName" placeholder="First name" required>
-
-                <input type="text" name="lastName" placeholder="Last name" required>
-
-                <input type="text" name="userName" placeholder="User name" required>
-
-                <input type="email" name="email" placeholder="Email" required>
-
-                <input type="email" name="email2" placeholder="Confirm email" required>
-
-                <input type="password" name="password" placeholder="Password" required>
-
-                <input type="password2" name="password2" placeholder="Confirm password" required>
-
-                <input type="submit" name="submitButton" value="  SUBMIT">
-            
-            </form>
 
         </div>
 
-    </div>
-</body>
+    </body>
 </html>
